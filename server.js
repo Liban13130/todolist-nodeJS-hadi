3830016757718743ce23cc5986b8bdef2360fe4b
Nabil Hellal <nabil-13130@hotmail.fr>
2024-07-28 11:46:46 +0200

    todo list

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', './Views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.render('accueil', { title: 'Accueil', message: 'Bienvenue sur mon site web!' });
+  });
+
+app.post('/submit', (req, res) => {
+  console.log(req.body.todo);
+  res.status(201).redirect('/')
+})
+
+app.listen(port, () => {
+    console.log(`Vous êtes branchées sur le port ${port}`);
+})
